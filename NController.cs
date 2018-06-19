d8911a6613ba5b5b220c663c3e0dbb30805eb95f
Dionell Macabugao <dionellmacabugao12@gmail.com>
2018-06-19 14:59:22 -0700

    Adding imported assets, resources, scenes, standard assets, textmesh pro, videos, and scripts

---
+++ NController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NController : MonoBehaviour {
+
+    public GameObject resetPosition;
+    public ParticleSystem shockwave;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            transform.position = resetPosition.transform.position;
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Flashlight")
+        {
+            shockwave.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Flashlight")
+        {
+            shockwave.Stop();
+        }
+
+    }
+}
---
+++ QuitGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Hand")
+        {
+            Application.Quit();
+        }
+    }
+}
